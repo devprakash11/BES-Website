10a488367693cb0bca9c8adfc84ed15271dc3d0e
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-09-15 23:33:10 +0530

    Load responsive reference homepage styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './app/App';
 import './styles/globals.css';
 import './styles/images.css';
 import './styles/classic.css';
+import './styles/reference.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>,
