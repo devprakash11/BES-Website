615d99a8b19866133c210decbd5f0f81692457ce
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-09-15 23:05:26 +0530

    fix: load responsive image styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import './styles/globals.css';
+import './styles/images.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>,
